b74ae5b5858d817b7e45812f60e532e84f07aa9e
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2024-01-27 18:47:43 -0500

    Primera parte

--- proyectoSolucionExoGrupo2/src/PJIABot.java
+++ proyectoSolucionExoGrupo2/src/PJIABot.java
@@ -1,30 +1,74 @@
-public class PJIABot{
+/**
+ * Clase que representa un bot que puede aprender español e inglés.
+ * Implementa las interfaces PJIAprendeEspaniol y PJIAprenderIngles.
+ * 
+ * @author Jonathan Paredes
+ * @Github Jonathan4325
+ * @version 1.0
+ */
+public class PJIABot implements PJIAprendeEspaniol, PJIAprenderIngles {
 
     private String pjcodigoSeguridad;
 
-
+    /**
+     * Constructor de la clase PJIABot.
+     * @param pjcodigoSeguridad 
+     */
     public PJIABot(String pjcodigoSeguridad) {
         this.pjcodigoSeguridad = pjcodigoSeguridad;
     }
-    public void pjVolarVeloz() {
-        System.out.println("Volando...");
+
+    /**
+     * Método para obtener el código de seguridad del bot.
+     * @return 
+     */
+    public String getPjcodigoSeguridad() {
+        return pjcodigoSeguridad;
     }
 
-    public void pjSaltarAlto() {
-        System.out.println("Saltando alto...");
-    }
-
-    public void pjCorrerRapido() {
-        System.out.println("Corriendo rapido...");
-    }
-
-    public void pjCrearCodigoSeguridad() {
-        setPjcodigoSeguridad("123456789");
-    }
-
+    /**
+     * Método para establecer el código de seguridad del bot.
+     * @param pjcodigoSeguridad El nuevo código de seguridad del bot.
+     */
     public void setPjcodigoSeguridad(String pjcodigoSeguridad) {
         this.pjcodigoSeguridad = pjcodigoSeguridad;
     }
 
+    /**
+     * Método para que vuele velozmente.
+     */
+    public void pjVolarVeloz() {
+        System.out.println("Volando velozmente...");
+    }
 
+    /**
+     * Método para que salte alto.
+     */
+    public void pjSaltarAlto() {
+        System.out.println("Saltando alto...");
+    }
+
+    /**
+     * Método para que corra rápido.
+     */
+    public void pjCorrerRapido() {
+        System.out.println("Corriendo rápido...");
+    }
+
+    /**
+     * Método para crear un código de seguridad predeterminado para el bot.
+     */
+    public void pjCrearCodigoSeguridad() {
+        setPjcodigoSeguridad("123456789");
+    }
+
+    @Override
+    public void pjAprenderEspaniol() {
+        System.out.println("Aprendiendo español...");
+    }
+
+    @Override
+    public void pjAprenderIngles() {
+        System.out.println("Aprendiendo inglés...");
+    }
 }
