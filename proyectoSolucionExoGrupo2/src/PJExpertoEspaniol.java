8d28e9e7d0c5aaca396340d03d1681592432d25e
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2024-01-28 15:06:47 -0500

    cambio

--- proyectoSolucionExoGrupo2/src/PJExpertoEspaniol.java
+++ proyectoSolucionExoGrupo2/src/PJExpertoEspaniol.java
@@ -1,48 +1,64 @@
-/**
- * Clase que implementa funcionalidades de un experto en español.
- * Permite enseñar léxico, gramática y fonética.
- * 
- * @author Jonathan Paredes
- * @Github Jonathan4325
- * @version 2.43
- */
-public class PJExpertoEspaniol implements PJIEnseniar{
+public class PJExpertoEspaniol implements PJIAprendeEspaniol,PJIEnsenia{
 
-    private String nombre;
-    private int edad;
+    private String pjnombre;
+    private int pjedad;
 
     /**
      * Constructor de la clase PJExpertoEspaniol.
-     * @param nombre El nombre del experto en español.
-     * @param edad La edad del experto en español.
+     * @param pjnombre El nombre del experto en español.
+     * @param pjedad La edad del experto en español.
      */
-    public PJExpertoEspaniol(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
+    public PJExpertoEspaniol(String pjnombre, int pjedad) {
+        this.pjnombre = pjnombre;
+        this.pjedad = pjedad;
     }
 
     /** getter/setter
      * 
      * @return
      */
-    public String getNombre() {
-        return nombre;
+    public String pjgetNombre() {
+        return pjnombre;
     }
 
     
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void pjsetNombre(String pjnombre) {
+        this.pjnombre = pjnombre;
     }
 
     
-    public int getEdad() {
-        return edad;
+    public int pjgetEdad() {
+        return pjedad;
     }
 
     
-    public void setEdad(int edad) {
-        this.edad = edad;
+    public void pjsetEdad(int pjedad) {
+        this.pjedad = pjedad;
     }
 
+    @Override
+    public void pjenseniarLexico() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pjenseniarLexico'");
+    }
+
+    @Override
+    public void pjenseniarGramatica() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pjenseniarGramatica'");
+    }
+
+    @Override
+    public void pjenseniarFonetica() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pjenseniarFonetica'");
+    }
+
+    @Override
+    public void pjAprenderEspaniol() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pjAprenderEspaniol'");
+    }
+
+   
 }
-
