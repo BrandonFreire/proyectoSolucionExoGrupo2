e55e873d1dd33dd0ac23d67e3b1c0758da4a45ac
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2024-01-28 20:42:46 -0500

    Comentado

--- proyectoSolucionExoGrupo2/src/PJIAprendeIngles.java
+++ proyectoSolucionExoGrupo2/src/PJIAprendeIngles.java
@@ -1,5 +1,11 @@
+/**
+ * Interfaz que define el método para aprender inglés.
+ */
 public interface PJIAprendeIngles {
 
+    /**
+     * Método para aprender inglés.
+     */
     void pjAprenderIngles();
 
 }
