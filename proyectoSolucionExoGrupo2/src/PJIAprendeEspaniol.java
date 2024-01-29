12d358b0b96952a1536e74adde431de10038eaa6
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2024-01-28 20:38:16 -0500

    comentado

--- proyectoSolucionExoGrupo2/src/PJIAprendeEspaniol.java
+++ proyectoSolucionExoGrupo2/src/PJIAprendeEspaniol.java
@@ -1,5 +1,11 @@
+/**
+ * Interfaz que define el método para aprender español.
+ */
 public interface PJIAprendeEspaniol {
 
+    /**
+     * Método para aprender español.
+     */
     void pjAprenderEspaniol();
 
 }
