3cea8ffb1cbdda8299f81927f3a5efe996fd599e
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2024-01-28 20:41:18 -0500

    comentada

--- proyectoSolucionExoGrupo2/src/PJIEnseniar.java
+++ proyectoSolucionExoGrupo2/src/PJIEnseniar.java
@@ -1,7 +1,20 @@
+/**
+ * Interfaz que define métodos para enseñar aspectos del idioma.
+ */
 public interface PJIEnseniar {
+    
+    /**
+     * Método para enseñar léxico del idioma.
+     */
     void pjEnseniarLexico();
 
+    /**
+     * Método para enseñar gramática del idioma.
+     */
     void pjEnseniarGramatica();
 
+    /**
+     * Método para enseñar fonética del idioma.
+     */
     void pjEnseniarFonetica();
 }
